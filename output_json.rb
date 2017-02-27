41e1147b25adf6ea4c28df0ae7dc43b92776a172
jverbosky <jverbosk@gmail.com>
2017-02-27 12:24:35 -0500

    added get_names() to output_json.rb, tweaked read_json() and write_json()

--- output_json.rb
+++ output_json.rb
@@ -1,13 +1,19 @@
 require 'json'
 
-def write_json(user_hash)
+def read_json()
   # if /public/user.json exists, read/parse its contents; otherwise set it to an empty array
   File.exist?('public/user.json') ? json = JSON.parse(File.read('public/user.json')) : json = []
+end
+
+def write_json(user_hash)
+  json = read_json()
   # append user_hash array to json data (existing or empty), then write beautified data to user.json
   File.open("public/user.json","w") { |f| f.puts JSON.pretty_generate(json << user_hash) }
 end
 
-def read_json()
-  json = JSON.parse(File.read('public/user.json'))  # read/copy the contents of the user.json file
+def get_names()
+  names = []
+  json = read_json()
+  json.each { |user_hash| names.push(user_hash["user_name"]) }
+  return names
 end
-
